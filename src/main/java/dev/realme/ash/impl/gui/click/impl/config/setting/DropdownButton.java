85363e5fcc0884a201d6c3d37e368c279276b6fe
xiguajerry <xiguajerry@outlook.com>
2024-07-19 18:56:08 +0800

    fix npe in DropdownButton

--- src/main/java/dev/realme/ash/impl/gui/click/impl/config/setting/DropdownButton.java
+++ src/main/java/dev/realme/ash/impl/gui/click/impl/config/setting/DropdownButton.java
@@ -7,6 +7,8 @@ import dev.realme.ash.impl.gui.click.impl.config.ModuleButton;
 import dev.realme.ash.init.Modules;
 import dev.realme.ash.util.string.EnumFormatter;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 import net.minecraft.client.gui.DrawContext;
@@ -30,14 +32,23 @@ public class DropdownButton extends ConfigButton<Enum<?>> {
     public void mouseClicked(double mouseX, double mouseY, int button) {
         if (this.isWithin(mouseX, mouseY)) {
             Enum<?> val = this.config.getValue();
-            Enum<?>[] constants = val.getClass().getEnumConstants();
+            Class<? extends Enum<?>> clz = (Class<? extends Enum<?>>) val.getClass();
+            Enum<?>[] constants = clz.getEnumConstants();
             if (constants == null) {
-                return;
+                clz = val.getDeclaringClass();
+                try {
+                    Method method = clz.getDeclaredMethod("$values");
+                    method.setAccessible(true);
+                    constants = (Enum<?>[]) method.invoke(null);
+                    if (constants == null) return;
+                } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
             }
             String[] values = Arrays.stream(constants).map(Enum::name).toArray(String[]::new);
             if (button == 0) {
                 this.index = this.index + 1 > values.length - 1 ? 0 : this.index + 1;
-                this.config.setValue(Enum.valueOf(val.getClass(), values[this.index]));
+                this.config.setValue(Enum.valueOf((Class) clz, values[this.index]));
             }
         }
 
